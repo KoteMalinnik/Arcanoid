7dab676cc1d9f525c82f8aa0e85cb490bd3806a8
Nikita Kalinnikov <niki9888985@mail.ru>
2020-08-08 10:53:16 +0300

    add MaxThreshold method to Extencions.cs
    противоположность методу MinThreshold

--- Assets/Scripts/Extencions.cs
+++ Assets/Scripts/Extencions.cs
@@ -7,10 +7,20 @@ public static class Extencions
     /// </summary>
     /// <param name="Value">Проверяемое значение.</param>
     /// <param name="threshold">Минимальный порог.</param>
-    /// <returns></returns>
     public static float MinThreshold(float Value, float threshold)
     {
         if (Value < threshold) return threshold;
+        return Value;
+    }
+
+    /// <summary>
+    /// Вернет значение <c>Value</c>, если оно не превосходит <c>threshold</c>. Иначе вернет <c>threshold</c>.
+    /// </summary>
+    /// <param name="Value">Проверяемое значение.</param>
+    /// <param name="threshold">Максимальный порог.</param>
+    public static float MaxThreshold(float Value, float threshold)
+    {
+        if (Value > threshold) return threshold;
         return Value;
     }
 }
