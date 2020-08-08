1546ba21a59947017ae39b60c5bad075ab5f41b3
Nikita Kalinnikov <niki9888985@mail.ru>
2020-08-08 10:39:34 +0300

    add platform movement (PlatformMovement.cs)

---
+++ Assets/Scripts/Platform/PlatformMovement.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour
+{
+	enum Direction
+    {
+		Left,
+		Right
+    }
+
+	#region Fields
+
+	[SerializeField] float movementSpeed = 1.0f;
+
+	[Header("Управление")]
+
+	[SerializeField] bool inverce = false;
+	[Space(10)]
+	[SerializeField] KeyCode keyToLeft = KeyCode.A;
+	[SerializeField] KeyCode keyToRight = KeyCode.D;
+
+	Transform cachedTransform = null;
+
+	#endregion
+	
+	#region Properties
+	
+	new Transform transform
+    {
+		get
+        {
+			if (cachedTransform == null) cachedTransform = base.transform;
+			return cachedTransform;
+        }
+    }
+
+    #endregion
+
+    #region MonoBehaviour Callbacks
+
+    private void OnValidate()
+    {
+		movementSpeed = Extencions.MinThreshold(movementSpeed, 0);
+
+		if (keyToLeft == KeyCode.None) Log.Warning("Клавиша перемещения платформы влево не назначена.");
+		if (keyToRight == KeyCode.None) Log.Warning("Клавиша перемещения платформы вправо не назначена.");
+	}
+
+    private void Update()
+    {
+		if (Input.GetKey(keyToLeft)) Move(Direction.Left);
+		if (Input.GetKey(keyToRight)) Move(Direction.Right);
+	}
+
+    #endregion
+
+	void Move(Direction direction)
+    {
+		float deltaPosition = movementSpeed * Time.deltaTime;
+		deltaPosition = direction == Direction.Left ? -deltaPosition : deltaPosition;
+
+		if (inverce) deltaPosition = -deltaPosition;
+
+		transform.position = new Vector2(transform.position.x + deltaPosition, transform.position.y);
+	}
+}
