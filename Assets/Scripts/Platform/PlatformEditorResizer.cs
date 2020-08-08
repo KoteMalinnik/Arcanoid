9b9e9009c51fe8bf133bd87dab139fefdd269c78
Nikita Kalinnikov <niki9888985@mail.ru>
2020-08-08 10:14:28 +0300

    add platform size changing from inspector

---
+++ Assets/Scripts/Platform/PlatformEditorResizer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PlatformEditorResizer : MonoBehaviour
+{
+	#region Fields
+
+	[SerializeField] float sizeX = 1f;
+	[SerializeField] float sizeY = 1f;
+
+    #endregion
+
+    #region Properties
+
+    public float SizeX => sizeX;
+    public float SizeY => sizeY;
+
+    #endregion
+
+    #region MonoBehaviour Callbacks
+
+    private void OnValidate()
+    {
+        sizeX = Extencions.MinThreshold(sizeX, 0.05f);
+        sizeY = Extencions.MinThreshold(sizeY, 0.05f);
+
+        ChangeSize();
+    }
+
+    #endregion
+
+    void ChangeSize()
+    {
+        Log.Message("Изменение размера платформы.");
+        transform.localScale = new Vector2(sizeX, sizeY);
+    }
+}
