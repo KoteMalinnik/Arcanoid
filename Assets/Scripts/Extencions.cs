9618cddc6b8c2eaba7bab22def5c483827fa9ba7
Nikita Kalinnikov <niki9888985@mail.ru>
2020-08-08 10:23:03 +0300

    add Extencions.cs with float MinThreshold(float, float) method

---
+++ Assets/Scripts/Extencions.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public static class Extencions
+{
+    /// <summary>
+    /// Вернет значение <c>Value</c>, если оно превосходит <c>threshold</c>. Иначе вернет <c>threshold</c>.
+    /// </summary>
+    /// <param name="Value">Проверяемое значение.</param>
+    /// <param name="threshold">Минимальный порог.</param>
+    /// <returns></returns>
+    public static float MinThreshold(float Value, float threshold)
+    {
+        if (Value < threshold) return threshold;
+        return Value;
+    }
+}
