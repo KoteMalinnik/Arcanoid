d05751ac280953a829a96136b64c3dd629c2dd03
Nikita Kalinnikov <niki9888985@mail.ru>
2020-08-09 13:31:21 +0300

    add rectBorder info class

---
+++ Assets/Scripts/BordersInfo/RectBorder.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class RectBorder
+{
+	#region Fields
+
+	Vector2 _TopLeftPoint = Vector2.zero;
+
+	#endregion
+
+	#region Properties
+
+	public Vector2 TopLeftPoint => _TopLeftPoint;
+
+	#endregion
+
+	public void UpdateBorder(Transform transform)
+	{
+		_TopLeftPoint.x = transform.position.x - transform.localScale.x / 2;
+		_TopLeftPoint.y = transform.position.y + transform.localScale.y / 2;
+	}
+
+	public void UpdateBorder(Vector2 topLeftPoint)
+	{
+		_TopLeftPoint = topLeftPoint;
+	}
+}
