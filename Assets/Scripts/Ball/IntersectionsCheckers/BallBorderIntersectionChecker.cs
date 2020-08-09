05aa8f75c2e888bfde87d3b884b2775f979be9b5
Nikita Kalinnikov <niki9888985@mail.ru>
2020-08-09 09:22:52 +0300

    remove BallMovement field
    проще получить доступ к родительскому трансформу.
    
    т.к. пересечение границы происходит не часто, нет неоходимости кешировать BallMovement

--- Assets/Scripts/Ball/IntersectionsCheckers/BallBorderIntersectionChecker.cs
+++ Assets/Scripts/Ball/IntersectionsCheckers/BallBorderIntersectionChecker.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using UnityEngine;
 
-[RequireComponent(typeof(BallMovement))]
 public class BallBorderIntersectionChecker : MonoBehaviour
 {
     #region Events
@@ -15,21 +14,30 @@ public class BallBorderIntersectionChecker : MonoBehaviour
 
     #region Fields
 
-    BallMovement ballMovement= null;
-
     float threshold_Left = 0;
     float threshold_Right = 0;
     float threshold_Top = 0;
     float threshold_Bottom = 0;
 
+    Transform cachedTransform = null;
+
     #endregion
 
     #region Properties
 
-    bool LeftBorderIntersection => ballMovement.transform.position.x < threshold_Left;
-    bool RightBorderIntersection => ballMovement.transform.position.x > threshold_Right;
-    bool TopBorderIntersection => ballMovement.transform.position.y > threshold_Top;
-    bool BottomBorderIntersection => ballMovement.transform.position.y < threshold_Bottom;
+    new Transform transform
+    {
+        get
+        {
+            if (cachedTransform == null) cachedTransform = base.transform.parent;
+            return cachedTransform;
+        }
+    }
+
+    bool LeftBorderIntersection => transform.position.x < threshold_Left;
+    bool RightBorderIntersection => transform.position.x > threshold_Right;
+    bool TopBorderIntersection => transform.position.y > threshold_Top;
+    bool BottomBorderIntersection => transform.position.y < threshold_Bottom;
 
     #endregion
 
@@ -37,24 +45,24 @@ public class BallBorderIntersectionChecker : MonoBehaviour
 
     private void Start()
     {
-        ballMovement = GetComponent<BallMovement>();
-
         LevelBorders.Initialize();
 
         //не недо проводить одни и те же рассчеты в *BorderIntersection-свойствах
-        Vector2 ballHalfSize = ballMovement.transform.localScale / 2;
-        threshold_Left = LevelBorders.border_Left + ballHalfSize.x;
-        threshold_Right = LevelBorders.border_Right - ballHalfSize.x;
-        threshold_Top = LevelBorders.border_Top - ballHalfSize.y;
-        threshold_Bottom = LevelBorders.border_Bottom + ballHalfSize.y;
+        Vector2 ballHalfSize = transform.localScale / 2;
+        
+        threshold_Left = LevelBorders.point_TopLeft.x + ballHalfSize.x;
+        threshold_Top = LevelBorders.point_TopLeft.y - ballHalfSize.y;
+
+        threshold_Right = LevelBorders.point_BottomRight.x - ballHalfSize.x;
+        threshold_Bottom = LevelBorders.point_BottomRight.y + ballHalfSize.y;
     }
 
     private void Update()
     {
         if (LeftBorderIntersection || RightBorderIntersection || TopBorderIntersection || BottomBorderIntersection)
         {
-            Log.Message($"Пересечение границы в позиции: {ballMovement.transform.position}.");
-            OnBorderIntersection?.Invoke(ballMovement);
+            Log.Message($"Пересечение границы. Позиция шара: {transform.position}.");
+            OnBorderIntersection?.Invoke(transform.parent.GetComponentInChildren<BallMovement>());
         }
     }
 
