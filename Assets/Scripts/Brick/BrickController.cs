e490accb39c64a2067ccb4d3db5d40847c11aed2
Nikita Kalinnikov <niki9888985@mail.ru>
2020-08-09 11:32:10 +0300

    add basic brick controller logic

---
+++ Assets/Scripts/Brick/BrickController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class BrickController : MonoBehaviour
+{
+	#region Fields
+
+	int durability = 1;
+
+    #endregion
+
+    #region Properties
+
+    int Durability
+    {
+        get { return durability; }
+        set
+        {
+            durability = value;
+            if (value <= 0) OnBrickDestroy();
+        }
+    }
+
+    #endregion
+    
+    public void SetLifes(int durability)
+    {
+        Log.Message($"Установка прочности ({durability}) кирпича {name}.");
+        if (durability <= 0)
+        {
+            Log.Warning($"Значение прочности некорректно. Установка прочности в 1.");
+            durability = 1;
+        }
+
+        this.durability = durability;
+    }
+
+    public void Hit()
+    {
+        Log.Message($"Удар по кирпичу: {name}. Прочность: {Durability - 1}.");
+        Durability--;
+    }
+
+    public void OnBrickDestroy()
+    {
+        Log.Message("Уничтожение кирпича: " + name);
+    }
+}
