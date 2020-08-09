7f4f3d45b61940763fc6fd9b547a2c479c8d378e
Nikita Kalinnikov <niki9888985@mail.ru>
2020-08-09 07:45:14 +0300

    add ball platform intersection checker
    необходимо знать, где шарик соприкасается с платформой

---
+++ Assets/Scripts/Ball/IntersectionsCheckers/BallPlatformIntersectionChecker.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class BallPlatformIntersectionChecker : MonoBehaviour
+{
+    #region Fields
+	
+	#endregion
+	
+	#region Properties
+	
+	#endregion
+	
+	#region MonoBehaviour Callbacks
+	
+	#endregion
+}
