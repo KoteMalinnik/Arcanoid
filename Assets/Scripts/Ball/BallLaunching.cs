a1450b46d673dccd37ed23813138945c2c0f28ad
Nikita Kalinnikov <niki9888985@mail.ru>
2020-08-09 07:41:29 +0300

    add ball launching system

---
+++ Assets/Scripts/Ball/BallLaunching.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BallMovement))]
+public class BallLaunching : MonoBehaviour
+{
+    enum OnLaunchDirection
+    {
+        ToLeftTop,
+        ToRightTop
+    }
+
+    #region Fields
+
+    [SerializeField] OnLaunchDirection onLaunchDirection = OnLaunchDirection.ToLeftTop;
+    [SerializeField] KeyCode launchKey = KeyCode.Space;
+
+    bool isLaunched = false;
+
+    #endregion
+
+    #region MonoBehaviour Callbacks
+
+    private void OnValidate()
+    {
+        if (launchKey == KeyCode.None) Log.Warning("Клавиша запуска шара не назначена.");
+    }
+
+    private void Start()
+    {
+        Log.Message("Ожидание нажатия кнопки запуска шара.");
+    }
+
+    private void Update()
+    {
+        if (isLaunched) return;
+
+        if (Input.GetKeyDown(launchKey))
+        {
+            Log.Message("Кнопка запуска была нажата.");
+
+            Launch();
+            isLaunched = true;
+        }
+    }
+
+    void Launch()
+    {
+        Log.Message("Запуск шара.");
+
+        var ballMovement = GetComponent<BallMovement>();
+
+        BallMovementDirections targetDirection = BallMovementDirections.ToLeftTop;
+
+        switch (onLaunchDirection)
+        {
+            case OnLaunchDirection.ToLeftTop:
+                targetDirection = BallMovementDirections.ToLeftTop;
+                break;
+            case OnLaunchDirection.ToRightTop:
+                targetDirection = BallMovementDirections.ToRightTop;
+                break;
+        }
+
+        ballMovement.SetDirection(targetDirection);
+        ballMovement.SetMovementPremission(true);
+    }
+    #endregion
+}
