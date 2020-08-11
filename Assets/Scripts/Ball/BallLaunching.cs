4451ff4df3445909456c3b0f422f5c8d355ca2ab
Nikita Kalinnikov <niki9888985@mail.ru>
2020-08-11 22:17:32 +0300

    add ball prelaunch position tracking

--- Assets/Scripts/Ball/BallLaunching.cs
+++ Assets/Scripts/Ball/BallLaunching.cs
@@ -1,12 +1,16 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(BallMovement))]
 public class BallLaunching : MonoBehaviour
 {
     #region Fields
 
     [SerializeField] KeyCode launchKey = KeyCode.Space;
-
     bool isLaunched = false;
+
+    Transform platformTransform = null;
+    Vector2 launchPosition = Vector2.zero;
+    float offsetY = 0;
 
     #endregion
 
@@ -20,34 +24,41 @@ public class BallLaunching : MonoBehaviour
     private void Start()
     {
         Log.Message("Ожидание нажатия кнопки запуска шара.");
+
+        platformTransform = FindObjectOfType<PlatformController>().transform;
+        offsetY = platformTransform.localScale.y / 2 + transform.localScale.y / 2;
+
+        UpdatePosition();
     }
 
     private void Update()
     {
         if (isLaunched) return;
 
+        UpdatePosition();
+
         if (Input.GetKeyDown(launchKey))
         {
             Log.Message("Кнопка запуска была нажата.");
-
             Launch();
         }
     }
 
     #endregion
 
+    void UpdatePosition()
+    {
+        launchPosition = platformTransform.position;
+        launchPosition.y += offsetY;
+        transform.position = launchPosition;
+    }
+
     void Launch()
     {
         Log.Message("Запуск шара.");
-
-        var ballMovement = transform.GetComponent<BallMovement>();
-        ballMovement.AllowMovement();
-
+        
+        GetComponent<BallMovement>().AllowMovement();
         isLaunched = true;
-    }
-    
-    public void Unlaunch()
-    {
-        isLaunched = false;
+        Destroy(this);
     }
 }
