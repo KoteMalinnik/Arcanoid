cf6fa88b7e41c88ad557271f4ca2082a8d72bbe2
Nikita Kalinnikov <niki9888985@mail.ru>
2020-08-11 11:07:49 +0300

    add Vector2 custom directions

---
+++ Assets/Scripts/Directions.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public static class Directions
+{
+    public static readonly Vector2 ToLeftTop = new Vector2(-1, 1);
+    public static readonly Vector2 ToLeftBottom = new Vector2(-1, -1);
+    public static readonly Vector2 ToRightTop = new Vector2(1, 1);
+    public static readonly Vector2 ToRightBottom = new Vector2(1, -1);
+}
