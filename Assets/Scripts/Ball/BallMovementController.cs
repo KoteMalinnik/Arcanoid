4703a3afe4a2ce1dc1fa1a7b2d4167bc0797ccc1
Nikita Kalinnikov <niki9888985@mail.ru>
2020-08-12 21:20:10 +0300

    add ball movement direction reflection

---
+++ Assets/Scripts/Ball/BallMovementController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System;
+
+[RequireComponent(typeof(BallMovement))]
+[RequireComponent(typeof(CircleCollider2D))]
+public class BallMovementController : MonoBehaviour
+{
+	#region Events
+
+	public static event Action OnBallTouchesBottom = null;
+
+	#endregion
+
+	#region Fields
+
+	BallMovement movement = null;
+
+	#endregion
+
+
+	#region MonoBehaviour Callbacks
+
+	private void Awake()
+	{
+		Component check = null;
+		if (!TryGetComponent(typeof(BallLaunching), out check)) gameObject.AddComponent<BallLaunching>();
+
+		movement = GetComponent<BallMovement>();
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+		if (collision.gameObject.name == "Bottom")
+        {
+			Log.Warning("КОНЕЦ ИГРЫ.");
+			movement.DisallowMovement();
+			OnBallTouchesBottom?.Invoke();
+			return;
+        }
+
+		if (collision.contactCount == 0)
+        {
+			Log.Warning("Контактов не обнаружено");
+			return;
+        }
+
+		var contact = collision.contacts[0];
+		var reflectedDirection = Vector2.Reflect(movement.Direction, contact.normal);
+
+#if UNITY_EDITOR
+		Debug.DrawRay(contact.point, contact.normal, Color.red, 5);
+		Debug.DrawRay(contact.point, -movement.Direction, Color.blue, 5);
+		Debug.DrawRay(contact.point, reflectedDirection, Color.green, 5);
+#endif
+
+		movement.SetDirection(reflectedDirection);
+    }
+
+    #endregion
+}
