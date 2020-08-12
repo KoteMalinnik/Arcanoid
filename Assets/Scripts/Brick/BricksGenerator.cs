a5d1b5e98f9b5bdb0bad3aaad70650ce2c591930
Nikita Kalinnikov <niki9888985@mail.ru>
2020-08-13 00:09:54 +0300

    add bricks generating

--- Assets/Scripts/Brick/BricksGenerator.cs
+++ Assets/Scripts/Brick/BricksGenerator.cs
@@ -6,6 +6,11 @@ public class BricksGenerator : MonoBehaviour
 
     [SerializeField] GameObject brickPrefab = null;
 
+    /// <summary>
+    /// Значение позиции по оси Y, ниже которого не будут генерироваться кирпичи.
+    /// </summary>
+    [SerializeField] float nonGeneratingBoundY = 0;
+
     [SerializeField] float offsetX = 0.1f;
     [SerializeField] float offsetY = 0.1f;
 
@@ -17,39 +22,67 @@ public class BricksGenerator : MonoBehaviour
     {
         offsetX = Extencions.MinThreshold(offsetX, 0);
         offsetY = Extencions.MinThreshold(offsetY, 0);
+
+        nonGeneratingBoundY = Extencions.MinThreshold(nonGeneratingBoundY, -2);
+        nonGeneratingBoundY = Extencions.MaxThreshold(nonGeneratingBoundY, Camera.main.orthographicSize);
+
+        Debug.DrawLine(
+            new Vector2(-Camera.main.orthographicSize*2, nonGeneratingBoundY),
+            new Vector2(Camera.main.orthographicSize*2, nonGeneratingBoundY),
+            Color.red, 0.01f);
     }
 
-    private void Awake()
+    public void Generate(int maxDurablity)
     {
+        maxDurablity = (int)Extencions.MinThreshold(maxDurablity, 1);
+
         Vector2 screenTopRightPoint = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
 
-        float screenWidth = 2 * screenTopRightPoint.x;
+        float screenHeight = 2 * screenTopRightPoint.y;
+        float brickScaleY = brickPrefab.transform.localScale.y;
+        float deltaY = offsetY + brickScaleY;
 
-        float brickScaleX = brickPrefab.transform.localScale.x;
-        float deltaX = offsetX + brickScaleX;
-        int bricksCountX = (int) (screenWidth / deltaX);
+        float allowGeneratingWindow = screenHeight / 2 - nonGeneratingBoundY;
+        int linesCount = (int)(allowGeneratingWindow / deltaY);
+        float screenSideOffset = (allowGeneratingWindow - (deltaY * linesCount - offsetY)) / 2;
 
-        Vector2 generatingPosition = Vector2.zero;
+        float generatingPositionY = screenTopRightPoint.y - screenSideOffset - brickScaleY / 2;
 
-        float screenOffset = (screenWidth - (deltaX * bricksCountX - offsetX)) / 2;
-        generatingPosition.x = -screenTopRightPoint.x + screenOffset + brickScaleX / 2;
-
-        for (int i = 0; i < bricksCountX; i++)
+        for (int i = 0; i < linesCount; i++)
         {
-            Debug.DrawRay(generatingPosition, Vector2.up, Color.white, 10);
-            generatingPosition.x += deltaX;
+            CreateLine(generatingPositionY, screenTopRightPoint, maxDurablity);
+            generatingPositionY -= deltaY;
         }
+
+        Destroy(this);
     }
 
     #endregion
 
-    BrickController CreateBreack(Vector2 position, int durablity)
+    void CreateLine(float positionY, Vector2 screenTopRightPoint, int maxDurablity)
+    {
+        float screenWidth = 2 * screenTopRightPoint.x;
+        float brickScaleX = brickPrefab.transform.localScale.x;
+        float deltaX = offsetX + brickScaleX;
+        int bricksCountX = (int)(screenWidth / deltaX);
+        float screenSideOffset = (screenWidth - (deltaX * bricksCountX - offsetX)) / 2;
+        
+        Vector2 generatingPosition = Vector2.zero;
+        generatingPosition.y = positionY;
+        generatingPosition.x = -screenTopRightPoint.x + screenSideOffset + brickScaleX / 2;
+
+        for (int i = 0; i < bricksCountX; i++)
+        {
+            CreateBrick(generatingPosition, Random.Range(1, maxDurablity));
+            generatingPosition.x += deltaX;
+        }
+    }
+
+    void CreateBrick(Vector2 position, int durablity)
     {
         Log.Message($"Создание кирпича в позиции {position}.");
 
         var brickController = Instantiate(brickPrefab, transform).GetComponent<BrickController>();
         brickController.Initialize(position, durablity);
-
-        return brickController;
     }
 }
