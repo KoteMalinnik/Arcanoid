4e341baefce9c53ddd6c506df18cdbbb4306b148
Nikita Kalinnikov <niki9888985@mail.ru>
2020-08-12 21:58:34 +0300

    add brick data with durability propertie

---
+++ Assets/Scripts/Brick/BrickData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+public class BrickData
+{
+    #region Properties
+
+    public int durability { get; private set; } = 0;
+
+    #endregion
+
+    public BrickData(int durability, GameObject parent)
+    {
+        if (parent == null) throw new ArgumentNullException();
+
+        Log.Message($"Установка прочности ({durability}) кирпича {parent.name}.");
+        if (durability <= 0)
+        {
+            Log.Warning($"Значение прочности некорректно. Установка прочности в 1.");
+            durability = 1;
+        }
+
+        this.durability = durability;
+    }
+
+    /// <summary>
+    /// Уменьшит прочность кирпича на 1 и вернет true, если прочность стала нулевой.
+    /// </summary>
+    public bool ReduceDurability()
+    {
+        durability--;
+        return durability <= 0;
+    }
+}
