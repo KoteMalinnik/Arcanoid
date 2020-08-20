2dc682dc6c7c42e6fd7da114c6599081b5d6fb52
Nikita Kalinnikov <niki9888985@mail.ru>
2020-08-20 23:15:27 +0300

    add bonus object movement script

---
+++ Assets/Scripts/Bonuses/BonusMovement.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class BonusMovement: MonoBehaviour
+{
+	#region Fields
+
+	Transform cachedTransform = null;
+	Vector2 newPosition = Vector2.zero;
+	float movementSpeed = 1;
+
+	#endregion
+
+	#region Properties
+
+	new Transform transform
+	{
+		get
+		{
+			if (cachedTransform == null) cachedTransform = base.transform;
+			return cachedTransform;
+		}
+	}
+
+	#endregion
+
+	#region MonoBehaviour Callbacks
+
+	private void Update()
+	{
+		newPosition.y -= movementSpeed * Time.deltaTime;
+		transform.position = newPosition;
+	}
+
+	private void OnBecameInvisible()
+	{
+		Log.Message("Касание платформы не было. Бонус вне экрана. Уничтожение.");
+		Destroy(gameObject);
+	}
+
+	#endregion
+
+	public void Initialize(float movementSpeed)
+	{
+		Log.Message("Инициализация движения объекта бонуса.");
+
+		this.movementSpeed = movementSpeed <= 0 ? 1 : movementSpeed;
+		newPosition = transform.position;
+
+		Log.Message("Скорость: " + this.movementSpeed);
+	}
+}
