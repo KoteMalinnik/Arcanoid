912b54fa6bea5faed685e62f4e05dfe918e38991
Nikita Kalinnikov <niki9888985@mail.ru>
2020-08-28 02:40:50 +0300

    add level data class

---
+++ Assets/Scripts/LevelController/LevelData.cs
@@ -0,0 +1,15 @@
+﻿public class LevelData
+{
+	#region Properties
+
+	public int maxBrickDurability { get; } = 1;
+	public bool generateBonuses { get; } = false;
+
+	#endregion
+
+	public LevelData(int maxBrickDurability, bool generateBonuses)
+    {
+		this.maxBrickDurability = maxBrickDurability > 0 ? maxBrickDurability : 1;
+		this.generateBonuses = generateBonuses;
+    }
+}
