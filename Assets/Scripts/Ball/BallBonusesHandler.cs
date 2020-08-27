60642a7bcd6630266b181053b73a7abe440fd3c8
Nikita Kalinnikov <niki9888985@mail.ru>
2020-08-28 01:57:45 +0300

    rename BallController -> BallBonusesHandler

---
+++ Assets/Scripts/Ball/BallBonusesHandler.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BallMovement))]
+public class BallBonusesHandler : MonoBehaviour
+{
+	#region Fields
+
+	BallMovement movement = null;
+
+    #endregion
+
+
+    #region MonoBehaviour Callbacks
+
+    private void OnEnable()
+    {
+		BonusPlatformTouchController.OnBonusReceive += Accelerate;
+		BonusPlatformTouchController.OnBonusReceive += Slowdown;
+	}
+
+    private void OnDisable()
+    {
+		BonusPlatformTouchController.OnBonusReceive -= Accelerate;
+		BonusPlatformTouchController.OnBonusReceive -= Slowdown;
+	}
+
+    private void Awake()
+	{
+		Component check = null;
+		if (!TryGetComponent(typeof(BallLaunching), out check)) gameObject.AddComponent<BallLaunching>();
+
+		movement = GetComponent<BallMovement>();
+	}
+
+    #endregion
+
+	void Accelerate(BonusType bonusType)
+    {
+		if (bonusType != BonusType.BallAcceleration) return;
+		
+		Log.Message("Ускорение.");
+		movement.ChangeSpeed(2);
+	}
+
+	void Slowdown(BonusType bonusType)
+    {
+		if (bonusType != BonusType.BallSlowdown) return;
+
+		Log.Message("Замедление.");
+		movement.ChangeSpeed(0.5f);
+	}
+}
