7130ff5800105ca16f2ca5c11fa816f8ef0ad578
Nikita Kalinnikov <niki9888985@mail.ru>
2020-08-28 02:41:25 +0300

    add level controller

---
+++ Assets/Scripts/LevelController/LevelController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+	#region Fields
+
+	[Range(1, 3)]
+	[SerializeField] int level = 1;
+
+    #endregion
+
+    #region MonoBehaviour Callbacks
+
+    private void Awake()
+    {
+        Log.Message("Уровень: " + level);
+        var levelData = Levels.GetLevelData(level);
+
+        GenerateBricks(levelData);
+        GenerateBonusesState(levelData);
+    }
+
+    #endregion
+
+    void GenerateBricks(LevelData levelData)
+    {
+        BricksGenerator bricksGenerator = GetComponentAtScene<BricksGenerator>();
+        bricksGenerator?.Generate(levelData.maxBrickDurability);
+    }
+
+    void GenerateBonusesState(LevelData levelData)
+    {
+        BonusesGenerator bonusesGenerator = GetComponentAtScene<BonusesGenerator>();
+        bonusesGenerator?.GenerateBonusesState(levelData.generateBonuses);
+    }
+
+    T GetComponentAtScene<T>() where T : MonoBehaviour
+    {
+        T sceneComponent = FindObjectOfType<T>(true);
+        if (sceneComponent == null)
+        {
+            Log.Error($"{typeof(T)} отсутствует на сцене.");
+            return null;
+        }
+
+        return sceneComponent;
+    }
+}
